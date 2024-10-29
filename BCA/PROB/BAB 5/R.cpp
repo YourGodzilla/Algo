3d8a0109d1828f746c1e7c073d4fd3d7604fb329
YourGodzilla <theo.yuriputra19@gmail.com>
2024-10-29 14:14:22 +0700

    #

--- BCA/PROB/BAB 5/R.cpp
+++ BCA/PROB/BAB 5/R.cpp
@@ -4,24 +4,24 @@
 int main () {
     int t;
     scanf("%d", &t);
+
     for (int tc = 1; tc <= t; tc++) {
-        char s[105];
-        scanf("%s", s);
+        char s[1005];
+        scanf("%s", s); getchar();
+
         int l = strlen(s);
+
         int m;
         scanf("%d", &m); getchar();
-        char a[1005], b[1005];
+
         for (int i = 0; i < m; i++) {
-            scanf("%c %c", &a[i], &b[i]); getchar();
+            char a, b;
+            scanf("%c %c", &a, &b); getchar();
             for (int j = 0; j < l; j++) {
-                if (s[j] == a[i]) {
-                    s[j] = b[i];
-                    // printf("%c", s[j]);
-                }
+                if (s[j] == a) s[j] = b;
             }
         }
         printf("Case #%d: %s\n", tc, s);
     }
-    
     return 0;
 }
