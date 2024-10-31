3a6cb938cedb6c6f4c261fd17f77e58939aefd06
YourGodzilla <theo.yuriputra19@gmail.com>
2024-10-31 10:43:48 +0700

    #

--- BCA/OJ/OLQ 8/C.cpp
+++ BCA/OJ/OLQ 8/C.cpp
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <string.h>
 
-int main () {
-    
+#define MOD 1000000007
+
+int main() {
+    FILE *file = fopen("testdata.in", "r");
+
+    char line[101];
+    long long totKosong = 0;
+
+    while (fgets(line, sizeof(line), file)) {
+        int l = strlen(line);
+        
+        for (int i = 0; i < l; i++) {
+            if (line[i] == ';') {
+                if (i == 0 || line[i - 1] == ';') {
+                    totKosong++;
+                }
+            }
+        }
+
+        if (line[l - 1] == ';') {
+            totKosong++;
+        }
+    }
+
+    long long totKesempatan = 1;
+    for (long long i = 0; i < totKosong; i++) {
+        totKesempatan = (totKesempatan * 101) % MOD;
+    }
+
+    printf("%lld\n", totKesempatan);
     return 0;
 }
