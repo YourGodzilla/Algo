d0784585cd86025a1d213431bfe566315f7e660b
YourGodzilla <theo.yuriputra19@gmail.com>
2024-11-08 10:03:25 +0700

    #

---
+++ BCA/OJ/BAB 5/Zblomfiks.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+    int t;
+    scanf("%d", &t);
+    for (int tc = 1; tc <= t; tc++) {
+        int n,k;
+        scanf("%d %d", &n, &k);
+        getchar();
+
+        char plot[n+5];
+        scanf("%s", plot);
+        getchar();
+
+        
+    }
+    
+    return 0;
+}
