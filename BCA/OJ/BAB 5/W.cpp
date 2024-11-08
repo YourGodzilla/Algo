511a17c34169e33404829b0133a9cacd8d13e76e
YourGodzilla <theo.yuriputra19@gmail.com>
2024-11-08 09:09:23 +0700

    #

---
+++ BCA/OJ/BAB 5/W.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main () {
+    int t;
+    scanf("%d", &t);
+    for (int tc = 1; tc <= t; tc++) {
+        int n;
+        scanf("%d", n);
+        int a[n+5];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            int found = 0;
+            for (int j = 0; j < n; j++) {
+                    for (int k = 0; k < n; k++) {
+                        if (k != j && a[i] == a[j] + a[k]) {
+                            found = 1;
+                            break;
+                        }
+                    }
+            if (found == 1) break;
+            }
+            if (found == 1) count++;
+        }
+        printf("Case #%d: %d\n", tc, count);
+    }
+    
+    return 0;
+}
