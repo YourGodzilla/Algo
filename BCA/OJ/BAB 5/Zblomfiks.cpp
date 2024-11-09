63f432e40d89dce7bcefdb6e6b10801356b5f714
YourGodzilla <theo.yuriputra19@gmail.com>
2024-11-09 13:18:58 +0700

    #

--- BCA/OJ/BAB 5/Zblomfiks.cpp
+++ BCA/OJ/BAB 5/Zblomfiks.cpp
@@ -1,19 +1,52 @@
 #include <stdio.h>
 
-int main () {
-    int t;
-    scanf("%d", &t);
-    for (int tc = 1; tc <= t; tc++) {
-        int n,k;
-        scanf("%d %d", &n, &k);
+int main() {
+    int T;
+    scanf("%d", &T);
+
+    for (int tc = 1; tc <= T; tc++) {
+        int N, K;
+        scanf("%d %d", &N, &K);
         getchar();
 
-        char plot[n+5];
+        char plot[N+5];
         scanf("%s", plot);
         getchar();
 
+        int stairCase = 0, firstStair = -1, lastStair = -1;
+        for (int i = 0; i < N; i++) { // menemukan tangga pertama
+            if (plot[i] == '1') {
+                firstStair = i + 1;
+                break;
+            }
+        }
+
+        for (int i = N - 1; i >= 0; i--) { // menemukan tangga terakhir
+            if (plot[i] == '1') {
+                lastStair = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < N; i++) { // menghitung total tangga
+            if (plot[i] == '1') {
+                stairCase++;
+            }
+        }
         
+        int flag = 0;
+        if (K >= N) {
+            flag = 1;
+        } else if (firstStair == -1) {
+            flag = 0;
+        } else if ((firstStair <= K) && (N - lastStair <= K) && (stairCase != 1)) {
+            flag = 1;
+        }
+
+        printf("Case #%d: ", tc);
+        if (flag == 1) printf("Alive\n");
+        else printf("Dead\n");
+
     }
-    
     return 0;
 }
