0a2a23c01d9d0d7be84b6bd4401455ed2802721b
YourGodzilla <theo.yuriputra19@gmail.com>
2024-11-13 13:21:38 +0700

    #

--- BCA/OJ/BAB 9/D.cpp
+++ BCA/OJ/BAB 9/D.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main () {
     FILE *file = fopen("testdata.in", "r");
@@ -8,7 +9,9 @@ int main () {
     for (int tc = 1; tc <= t; tc++) {
         char s[105];
         fscanf(file, "%[^\n]\n", s);
+        int l = strlen(s);
         int stc;
+        char s1[105];
     }
     return 0;
 }
