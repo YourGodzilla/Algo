07b840bd3abb2fba67d4c2759aff800db86adf93
YourGodzilla <theo.yuriputra19@gmail.com>
2024-11-14 13:48:05 +0700

    #

--- BCA/OJ/OLQ 9/B.cpp
+++ BCA/OJ/OLQ 9/B.cpp
@@ -1,35 +1,46 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
 
-int ascending_order;
-
-int compare(const void *a, const void *b) {
-    const char *strA = *(const char **)a;
-    const char *strB = *(const char **)b;
-    return ascending_order ? strcmp(strA, strB) : strcmp(strB, strA);
+void bubble_sort(char words[][100], int n, int ascending) {
+    char temp[100];
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (ascending == 1) {
+                if (strcmp(words[j], words[j + 1]) > 0) {
+                    strcpy(temp, words[j]);
+                    strcpy(words[j], words[j + 1]);
+                    strcpy(words[j + 1], temp);
+                }
+            }
+            else {
+                if (strcmp(words[j], words[j + 1]) < 0) {
+                    strcpy(temp, words[j]);
+                    strcpy(words[j], words[j + 1]);
+                    strcpy(words[j + 1], temp);
+                }
+            }
+        }
+    }
 }
 
 int main() {
-    int n;
+    int n, x;
     scanf("%d", &n);
-    
-    char nama[n][55];
-    char *namaPointers[n];
-    
+
+    char words[n][100];
+
     for (int i = 0; i < n; i++) {
-        scanf("%s", nama[i]);
-        namaPointers[i] = nama[i];
+        scanf("%s", words[i]);
     }
-    
-    int order;
-    scanf("%d", &order);
-    ascending_order = (order == 0);
 
-    qsort(namaPointers, n, sizeof(char *), compare);
+    scanf("%d", &x);
+
+    int ascending = (x == 0);
+
+    bubble_sort(words, n, ascending);
 
     for (int i = 0; i < n; i++) {
-        printf("%s\n", namaPointers[i]);
+        printf("%s\n", words[i]);
     }
 
     return 0;
--- BCA/OJ/OLQ 9/C.cpp
+++ BCA/OJ/OLQ 9/C.cpp
@@ -14,7 +14,7 @@ void swap(Data *a, Data *b) {
 
 int partition(Data arr[], int low, int high) {
     int pivot = arr[high].num;
-    int i = low - 1;
+    int i = low - 1; 
 
     for (int j = low; j < high; j++) {
         if (arr[j].num > pivot || (arr[j].num == pivot && strcmp(arr[j].name, arr[high].name) < 0)) {
@@ -22,13 +22,14 @@ int partition(Data arr[], int low, int high) {
             swap(&arr[i], &arr[j]);
         }
     }
+    
     swap(&arr[i+1], &arr[high]);
     return (i + 1);
 }
 
 void quicksort(Data arr[], int low, int high) {
     if (low < high) {
-        int pi = partition(arr, low, high);
+        int pi = partition(arr, low, high); 
 
         quicksort(arr, low, pi-1);
         quicksort(arr, pi+1, high);
@@ -36,10 +37,11 @@ void quicksort(Data arr[], int low, int high) {
 }
 
 int main() {
-    Data data[105];
-
     int n;
     scanf("%d", &n);
+
+    Data data[n];
+
     for (int i = 0; i < n; i++) {
         scanf("%s %d", data[i].name, &data[i].num);
     }
@@ -49,4 +51,6 @@ int main() {
     for (int i = 0; i < n; i++) {
         printf("%s %d\n", data[i].name, data[i].num);
     }
+
+    return 0;
 }
