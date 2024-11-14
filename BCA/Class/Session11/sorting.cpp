a14d724709f748f0445b24e02b1f11b786e3602c
YourGodzilla <theo.yuriputra19@gmail.com>
2024-11-14 10:03:56 +0700

    #

--- BCA/Class/Session11/sorting.cpp
+++ BCA/Class/Session11/sorting.cpp
@@ -27,6 +27,25 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
+void countingSort(int arr[], int n, int k) {
+    int count[k+5];
+    for (int i = 0; i < k+5; i++) {
+        count[i] = 0;
+    }
+    
+    for (int i = 0; i < n; i++) { // counting
+        count[arr[i]]++;
+    }
+    
+    int m = 0;
+    for (int i = 0; i < k + 5; i++) {
+        for (int j = 0; j < count[i]; j++) {
+            arr[m] = i;
+            m++;
+        }
+    }
+}
+
 int main () {
     int angka[1005];
     printf("Before Sorting: \n");
@@ -36,8 +55,9 @@ int main () {
     }
     printf("\n");
     
-    selectionSort(angka, 20);
+    // selectionSort(angka, 20);
     // bubbleSort(angka, 20);
+    countingSort(angka, 20, 1001); 
 
     printf("After Sorting: \n");
     for (int i = 0; i < 20; i++) {
