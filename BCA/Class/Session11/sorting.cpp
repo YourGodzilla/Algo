077459300d90010323447809fa5a6f257e570081
YourGodzilla <theo.yuriputra19@gmail.com>
2024-11-14 09:01:10 +0700

    #

--- BCA/Class/Session11/sorting.cpp
+++ BCA/Class/Session11/sorting.cpp
@@ -9,7 +9,7 @@ void swap(int *a, int *b) {
 
 void selectionSort(int arr[], int n) {
     for (int i = 0; i < n - 1; i++) {
-        int k = 1;
+        int k = i;
         for (int j = i + 1; j < n; j++) {
             if (arr[j] < arr[k]) k = j;
         }
@@ -17,19 +17,30 @@ void selectionSort(int arr[], int n) {
     }
 }
 
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+}
+
 int main () {
     int angka[1005];
     printf("Before Sorting: \n");
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 20; i++) {
         angka[i] = rand() % 1000 + 1;
         printf("%d, ", angka[i]);
     }
     printf("\n");
     
     selectionSort(angka, 20);
+    // bubbleSort(angka, 20);
 
     printf("After Sorting: \n");
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 20; i++) {
         printf("%d, ", angka[i]);
     }
     printf("\n");
