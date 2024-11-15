837379796d4a0061710c4ee926980e6429dff22e
YourGodzilla <theo.yuriputra19@gmail.com>
2024-11-15 11:04:49 +0700

    #

--- BCA/Class/LAB/Class/15Nov2024/new.cpp
+++ BCA/Class/LAB/Class/15Nov2024/new.cpp
@@ -8,6 +8,7 @@ typedef struct {
 ;} Item;
 
 Item itemList[100];
+int counter = 0;
 
 void createItem();
 void viewItem();
@@ -82,5 +83,48 @@ void viewItem() {
 }
 
 void deleteItem() {
+    FILE *file = fopen("items.txt", "r");
 
+    Item item;
+    Item deletedItem;
+    int found;
+
+    if (file == NULL) {
+        printf("File not found!\n");
+        return;
+    }
+
+    char toDelete[30];
+    printf("Enter item to delete: ");
+    scanf("%[^\n]", toDelete); getchar();
+
+    while(fscanf(file, "%[^#]#%d#%d\n", item.name, &item.price, &item.stock) != EOF) {
+        if (strcmp(item.name, toDelete) != 0 || found == 1) {
+            strcpy(itemList[counter].name, item.name);
+            itemList[counter].price = item.price;
+            itemList[counter].stock = item.stock;
+            counter++;
+        } else {
+            strcpy(deletedItem.name, item.name);
+            deletedItem.price = item.price;
+            deletedItem.stock = item.stock;
+            found = 1;
+        }
+    }
+    fclose(file);
+
+    if (found) {
+        FILE *file = fopen("items.txt", "w");
+
+        for (int i = 0; i < counter; i++) {
+            fprintf(file, "%s#%d#%d\n", itemList[i].name, itemList[i].price, itemList[i].stock);
+        }
+        fclose(file);
+        
+        printf("%s %d %d has been deleted!\n", deletedItem.name, deletedItem.price, deletedItem.stock);
+    } else {
+        printf("Item not found!\n");
+    }
+
+    counter = 0;
 }
