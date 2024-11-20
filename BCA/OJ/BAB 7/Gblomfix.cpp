5c1abcca479ef843fbfa2575be8d18591868aa8f
YourGodzilla <theo.yuriputra19@gmail.com>
2024-11-20 09:25:59 +0700

    #

--- BCA/OJ/BAB 7/Gblomfix.cpp
+++ BCA/OJ/BAB 7/Gblomfix.cpp
@@ -1,5 +1,30 @@
 #include <stdio.h>
 
+void swap (int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int partition (int arr[], int low, int high) {
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= arr[high]) {
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i+1], &arr[high]);
+    return i + 1;
+}
+
+void quicksort (int arr[], int low, int high) {
+    int pivot = partition(arr, low, high);
+
+    quicksort(arr, low, pivot - 1);
+    quicksort(arr, pivot + 1, high);
+}
+
 int main () {
     
 }
