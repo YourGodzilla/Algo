80253d2ee750ecfc0e5eff1380d2a71720d9516b
YourGodzilla <theo.yuriputra19@gmail.com>
2024-12-06 13:30:58 +0700

    #

---
+++ Cawu 1/Class/Binus5Des2024/Lab/menu.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    // menu
+    int choice;
+    do {
+        
+    } while (choice < 1 || choice > 4);
+
+    return 0;
+}
